076881c8af836754ea2fc56b726b0b165f2895b8
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-05 00:06:24 +0700

    commit study

---
+++ input.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class input {
+    
+
+    public static void main(String[] args) {
+        Scanner objct=new Scanner(System.in);
+        System.out.print("masukkan nama: ");
+        String nama=objct.nextLine();
+
+        System.out.println("nama kamu adalah " + nama);
+
+        System.out.print("masukkan umur: ");
+        int age=objct.nextInt();
+
+        if (age>=17){
+            System.out.println("kamu sudah dewasa");
+        }else{
+            System.out.println("kamu belum dewasa");
+        }
+
+        System.out.println("masukkan angka: ");
+        int num=objct.nextInt();
+
+        if(num%2==0){
+            System.out.println("bilangan bulat");
+        }else{
+            System.out.println("sisa angka: " + num%2);
+        }
+        
+    }
+}
