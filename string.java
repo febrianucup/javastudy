6ff7c59a7c863585196097261fcd11a5370eb902
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-05 20:08:23 +0700

    string

---
+++ string.java
@@ -0,0 +1,17 @@
+public class string {
+    public static void main(String[] args) {
+        
+        //indexof-index (the position) of the first occurrence of a specified text in a string (including whitespace)
+        String alpabhet="abcdrfghij";
+        System.out.println(alpabhet.indexOf("c"));
+
+        //typeuppercase untuk mengubah string menjadi huruf besar
+        System.out.println(alpabhet.toUpperCase());
+        //typelowercase untuk mengubah string menjadi huruf kecil
+        System.out.println(alpabhet.toLowerCase());
+        
+        //length untuk mengukur panjang sebuah string
+        System.out.println(alpabhet.length());
+    }
+}
+
