d662db352797d273cf561367c6f3366380d11df3
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-05 21:04:30 +0700

    control statement

--- controlstatement.java
+++ controlstatement.java
@@ -1,6 +1,37 @@
-class sontorlstatemnt{
+import java.util.Scanner;
+
+class contorlstatemnt{
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-    }
+        Scanner inTxt=new Scanner(System.in);
+        int num=inTxt.nextInt();
+        //if, else, else...if
+        //if
+        if(num==10){
+            System.out.println("hello world");
+        }
+
+        // else
+        if(num==20){
+            System.out.println("hello kang");
+        }else{
+            System.out.println("hello borok");
+        }
+
+        //else..if
+        if(num % 2==0){
+            System.out.println("ini angka genap");
+        }else if (num % 2 !=0) {
+            System.out.println("ini angka ganjil");
+        }else{
+            System.out.println("bukan keduanya");
+        }
+
+        //short hand if...else
+        String result=(num-5==0) ? "hasilnya 0" : "hasilnya bukan 0";
+        System.out.println(result);
+
+        //switch
+       
+    }    
 }
