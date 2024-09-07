d9e8d64f6196fa8cee7e85e77ecea43af19e738a
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-07 15:46:21 +0700

    multiple varable

--- variabel.java
+++ variabel.java
@@ -8,5 +8,12 @@ public class variabel {
         boolean myBool=true;
         float myFloat=20.2f;
         System.out.println(str + "\n" +  num + "\n" + character + "\n" + myBool + "\n"+ myFloat);
+
+        //multiple variabel
+        int x, y, z;
+        x=2;
+        y=3;
+        z=4;
+        System.out.println(x + y + z);
     } 
 }
