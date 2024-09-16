5f4e059c0df4296990e1f9a9b08e6b96395db81f
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-16 19:35:37 +0700

    error

--- controlstatement.java
+++ controlstatement.java
@@ -31,17 +31,34 @@ class contorlstatemnt{
         String result=(num-5==0) ? "hasilnya 0" : "hasilnya bukan 0";
         System.out.println(result);
 
+        String hasil=(num!=10) ? "hasillnya bukan 10":"hasilnya adalah 10";
+        System.out.println(hasil);
+
         //switch
         int kelas=2;
         switch (kelas) {
             case 1 :
                 System.out.println("kamu sd kelas 1");
-                break;
+                break;//break digunakan untuk menghentikan output ketika condition telah benar
             case 2 :
                 System.out.println("kamu sd kelas 2");
                 break;
             default:
                 System.out.println("kmau kelas berapa?");
+       }
+       
+       int x=4;
+       switch(x){
+            case 1 :
+                System.out.println("nomor 1");
+                break;
+            case 2 :
+                System.out.println("nomor 2");
+                break;
+            case 3 :
+                System.out.println("nomor 3");
+            default:
+                System.out.println("default");
        }
     }    
 }
