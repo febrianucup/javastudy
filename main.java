360296757097fb29714cbab842a53d2a809cc726
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-17 20:10:35 +0700

    wriboy

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n = 85;
+
+        if ( 90 < n ) {
+            System.out.println("A");
+        }else if (n > 80){
+            System.out.println("B");
+        }else if (n > 70 ){
+            System.out.println("C");
+        }else if (n < 70 ){
+            System.out.println("Tidak Lulus");
+        }else{
+            
+        }
+        
+    }
+    }
