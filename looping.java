de41739482bee38096acb24b47a6d9c6b7a9180e
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-24 09:17:19 +0700

    number of prime

--- looping.java
+++ looping.java
@@ -1,34 +1,63 @@
+
+import java.util.Scanner;
+
+
 public class looping {
 
     public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+
+
         //looping while, do while
-        int i=0;
-        //while
-        while (i<6) {
-            System.out.println(i);
-            i++;
-        }
+        // int i=0;
+        // //while
+        // while (i<6) {
+        //     System.out.println(i);
+        //     i++;
+        // }
         //do while
-        int j=0;
-        do { 
-            System.out.println(j);
-            j++;
-        } while (j<10);
+        // int j=0;
+        // do { 
+        //     System.out.println(j);
+        //     j++;
+        // } while (j<10);
 
         //for loop
-        for (int k=0; k<=5; k+=1){
+        for (int k=0; k<=5; k++){
             System.out.println(k);
         }
 
-        //Nested loops
-        //Outer loop
-        for (int u=1; u<=5; u++){
+        // Nested loops
+        // Outer loop
+        for (int u=1; u<=4; u++){
             System.out.println("outer: " + u);
 
             //Inner loop
-            for (int r=1; r<=4; r++){
+            for (int r=1; r<=u; r++){
                 System.out.println("inner: " + r);
             }
         }
+
+        // //example
+        // for(int i=1; i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("+");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int A=input.nextInt();
+        // int noOfFactor=0;
+        // for(int k=1;k<=A;k++){
+        //     int countOfFactor=0;
+        //     for(int l=1;l<=k;l++){
+        //         if(k%l==0){
+        //             countOfFactor++;
+        //         }
+        //     }
+        //     if(countOfFactor==2){
+        //         noOfFactor++;   
+        //     }
+        // }
     }
 }
